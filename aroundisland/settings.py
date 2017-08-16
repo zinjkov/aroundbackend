4604bded504c367b0a17f465ac9e5c297d4de961
zinjkov <zinjkov.su@gmail.com>
2017-08-16 11:12:48 +1000

    added 127.. allowed host

--- aroundisland/settings.py
+++ aroundisland/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@fr=wg!$4u))(q^-%^-uxzfa93%7&mtuku=*%(x6h+j*3j6=5='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aroundisland.ru', 'localhost']
+ALLOWED_HOSTS = ['aroundisland.ru', 'localhost', '127.0.0.1']
 
 
 # Application definition
